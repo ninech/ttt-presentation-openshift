a9445c8a9d07b91d15b9e4c6698b6797a2c521fa
Philippe Hässig <phil@nine.ch>
2017-02-24 16:51:49 +0100

    Improve start script for Openshift

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-npm install
+echo "---> Installing nodejs dependencies"
+if npm ls &>/dev/null;
+then
+  echo "All NodeJS dependencies are installed."
+else
+  npm install
+fi
+
+echo "---> Starting the server"
 exec npm start
